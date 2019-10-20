f65a71a95ac60a2a7de1637864f6dc63697fa06c
giovannic96 <giovanni.cala@hotmail.it>
2019-10-20 21:02:26 +0200

    added broadcast method in serverMaster and relative JSON response in Service

--- Source/server/src/main/java/it/polito/se2/server/ServerMaster.java
+++ Source/server/src/main/java/it/polito/se2/server/ServerMaster.java
@@ -1,21 +1,22 @@
 package it.polito.se2.server;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.LinkedList;
-
+import java.util.ArrayList;
+import org.json.JSONObject;
 import it.polito.se2.database.DatabaseMaster;
 
 public class ServerMaster {
 	private ServerSocket serverSocket;
-	private LinkedList<ServerWorker> allConnections = new LinkedList<>();
 	private DatabaseMaster database;
 	private Connection connection;
 	public static final int PORT_NUMBER = 1500;
 	public static final String DATABASE_CONFIG = "database.properties";
+	public static ArrayList<Socket> sockets = new ArrayList<Socket>();
 	
 	public ServerMaster(int portNumber) throws SQLException {
 		try	{
@@ -42,9 +43,9 @@ public class ServerMaster {
 			System.out.println("Waiting for clients to connect...");
 			try	{
 				Socket client = serverSocket.accept();
-				ServerWorker worker = new ServerWorker(client, connection);
+				sockets.add(client);
+				new ServerWorker(client, connection);
 				System.out.println("New connection");
-				allConnections.add(worker);
 			} catch (IOException e)	{
 				System.err.println("Server error: Failed to connect to client.");
 				e.printStackTrace();
@@ -52,12 +53,16 @@ public class ServerMaster {
 		}
 	}
 
-	public void broadcast(String s)	{
-		for (ServerWorker workers : allConnections)
-		{
-
-		}
-	}
+	public static void broadcast(JSONObject message) {
+        try {
+            for (Socket socket : sockets) {
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                out.println(message);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 	public static void main(String args[]) throws SQLException {
 		new ServerMaster(PORT_NUMBER);
